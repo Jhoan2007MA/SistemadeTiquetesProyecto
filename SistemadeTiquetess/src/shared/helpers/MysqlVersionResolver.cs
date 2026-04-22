1dfa44172d6f12c0b9f502293f76ef8224e36c39
Jhoan2007MA <217262098+Jhoan2007MA@users.noreply.github.com>
2026-04-22 00:26:28 -0500

    feat: :sparkles: Commit 11
    Adicion de contenido
    
    Adicion de contenido en los archivos DbContex y Mysqlversion

--- SistemadeTiquetess/src/shared/helpers/MysqlVersionResolver.cs
+++ SistemadeTiquetess/src/shared/helpers/MysqlVersionResolver.cs
@@ -4,5 +4,12 @@ namespace SistemadeTiquetess.src.shared.helpers;
 
 public class MysqlVersionResolver
 {
-
+    public static Version DetectVersion(string connectionString)
+    {
+        using var conn = new MySqlConnection(connectionString);
+        conn.Open();
+        var raw = conn.ServerVersion;
+        var clean = raw.Split('-')[0];
+        return Version.Parse(clean);
+    }
 }
