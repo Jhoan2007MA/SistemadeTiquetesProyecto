8f0d9e62ec747920d7474fb90475900eef6bea78
Jhoan2007MA <217262098+Jhoan2007MA@users.noreply.github.com>
2026-04-23 01:41:58 -0500

    feat: :sparkles: Commit 50
    Actulizacion y creacion de archivos

--- SistemadeTiquetess/src/modules/Airlines/Application/AirlineDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemadeTiquetess.src.modules.Airlines.Application;
-
-public class AirlineDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Code { get; set; } = string.Empty;
-}
--- SistemadeTiquetess/src/modules/Airlines/Domain/Airline.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SistemadeTiquetess.src.modules.Airlines.Domain;
-
-public class Airline
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Code { get; set; } = string.Empty;
-}
